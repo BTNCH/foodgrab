5867b325d54745a0dd19ef2df50f0df5fa0c45d6
Tanachart <614259044@webmail.npru.ac.th>
2020-10-05 14:03:33 +0700

    finish commit

---
+++ data.js
@@ -0,0 +1,65 @@
+const restaurants=[
+{
+    id : 1,
+    name : "จิวโจ๊กบางลำพู สุกี้กะทะร้อน - ถนนตานี",
+    type : "ร้านอาหารเส้น",
+    imageURL : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST000001zo/hero/upload-photo-Hero_Photo_cb41e8148e50410a8f33632e0c336c02_1552533003406029094.jpeg"
+},
+{
+    id : 2,
+    name : "ไก่ไม่ยอมนอน - ซอยรามบุตรี",
+    type : "ร้านข้าวหน้า",
+    imageURL : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZLDNAATVA2UNJ/hero/cab978976ec14ec3bffc196e49fcd919_1594554201623013383.png"
+},
+{
+    id : 3,
+    name : "นายอ้วนเย็นตาโฟเสาชิงช้า - ถนนตะนาว",
+    type : "ร้านอาหารเส้น",
+    imageURL : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST0000022y/hero/c4b9c9ca49f84274861dc367529d3cc0_1592216282560213295.jpeg"
+},
+{
+    id : 4,
+    name : "Kamu Tea (คามุ ที) - แอม ไชน่าทาวน์",
+    type : "ร้านชานมไข่มุก",
+    imageURL : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZAENBUFV63ZBE/hero/dd294a57c8b04d3f83429be4c437fec5_1580462688816273956.jpeg"
+},  
+ {
+    id : 5,
+    name : "Passion Food อาหารคลีนเดลิเวอรี่ - สุขุมวิท",
+    type : "ร้านอาหารสุขภาพ",
+    imageURL : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZCEL7JKPAWGTT/hero/4e69a04f39db436b99498b5929cf0984_1596162680320444445.jpeg"
+},    
+{
+    id : 6,
+    name : "แพร่งนมสด - เสาชิงช้า",
+    type : "ร้านกาแฟ/ชา",
+    imageURL : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST0000021s/hero/upload-photo-Hero_Photo_12e479b8999044a183e5f9660db198b0_1549616241374620352.jpeg"
+},   
+ {
+    id : 7,
+    name : "Pad Thai Ekamai (ผัดไทเอกมัย) - พระนคร(ข้าวสาร)",
+    type : "ร้านผัดไทย",
+    imageURL : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CY4JLYDCVFCWHE/hero/51ca1ddc3f504270bd9a63b3cb65877b_1596379557902379882.jpg"
+},    
+{
+    id : 8,
+    name : "พอดีคำ เมี่ยงหมิวสลัดโรล - ถนนทรงพล",
+    type : "ร้านอาหารสุขภาพ",
+    imageURL : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZNDLNJEG6MZJ6/hero/1ad1d01ec62b42d28e6b80a1873235a2_1598680317338129190.jpeg"
+},  
+{
+    id : 9,
+    name : "Pizza Hut (พิซซ่าฮัท) - ถนนบำรุงเมือง",
+    type : "ร้านพิซซ่า",
+    imageURL : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CYTDJJE1VY2BEJ/hero/132234b8dcfc4fc7aee69a0ffdf08f80_1600675134589446721.jpg"
+},  
+ 
+{
+    id : 10,
+    name : "Indian Kebab & Curries - ถนนพระอาทิตย์",
+    type : "ร้านอาหารตามสั่ง",
+    imageURL : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZEUVJWBCK4GVJ/hero/26f62a787d2f426c96d3acbc012b8f75_1590218946857198659.jpeg"
+}
+]
+
+module.exports = restaurants;
